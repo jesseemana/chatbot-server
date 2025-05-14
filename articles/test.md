6315dcca76cab122c91f01ddd562a64968f50945
Jesse Emana <jesseemana@gmail.com>
2025-05-14 21:41:05 +0200

    CHORE: did some stuff

---
+++ articles/test.md
@@ -0,0 +1,19 @@
+Welcome the MBC Web Portal 2.0
+
+# Technologies Used
+This application is built using React 19 + Vite + Tanstack Router.
+
+# To Run On Your Machine
+To run this project on your machine:
+```
+npm install
+```
+```
+npm run dev
+```
+
+# To Build For Production
+To build this project for production:
+```
+npm run build
+```
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import 'dotenv/config'
 import express, { Request, Response } from 'express'
-import z from 'zod'
 import cors from 'cors'
+import z from 'zod'
 import OpenAI from 'openai'
-import { validateInput } from './validation'
+import fs from 'fs'
+import pdf from 'pdf-parse'
+import { validateInput } from '../utils/validation'
+import { connectDatabase } from '../utils/database'
+import { upload } from '../utils/upload'
 
 const server = express()
 const PORT = parseInt(process.env.PORT as string) || 8080
@@ -12,40 +16,50 @@ const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY as string,
 })
 
-/**
- * START Zod schema stuff.
- * Create a zod schema which will be used when sanitizing the user input.
- */
+// The Zod schema definition
 const chatInputSchema = z.object({
   body: z.object({
     input: z
-      .string({ required_error: 'Please provide a prompt.' })
+      .string({
+        required_error: 'Please provide a prompt.'
+      })
       .min(2, 'Prompt must be at least 2 letters.')
     }
   )
 })
 
 type ChatInput = z.infer<typeof chatInputSchema>
-/**
- * END Zod schema stuff.
- */
 
-
-/**
- * Start Middleware
- */
+// Start Middleware
 server.use(cors())
 server.use(express.json())
-/**
- * End Middleware
- */
 
-
-/**
- * Start API Routes
- */
+// Health Check
 server.get('/healthz', (_req, res) => {
   res.status(200).json({ msg: 'Health OK!' })
+})
+
+server.post('/api/v1/upload', upload.single('file'), async (req, res) => {
+  try {
+    const file = req.file
+    const uploaded = fs.readFileSync(`./uploads/${file?.filename}`, 'utf-8')
+
+    if (uploaded) {
+      const buffer = Buffer.from(uploaded)
+      console.log(buffer)
+
+      const data = await pdf(buffer)
+      // console.log(data.text)
+
+      // TODO: save the text in database 
+      res.status(200).json({ msg: 'File read successfully' })
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('An error occurred.', error.message)
+    }
+    res.status(500).json({ msg: 'Internal Server Error.' })
+  }
 })
 
 server.post('/api/v1/chat', validateInput(chatInputSchema), async (
@@ -54,10 +68,37 @@ server.post('/api/v1/chat', validateInput(chatInputSchema), async (
 ) => {
   try {
     const { input } = req.body
+
     const results = await client.responses.create({
-      model: 'gpt-4o-mini',
-      input
+      model: 'chatgpt-4o-latest',
+      input,
+      // stream: true
     })
+
+    // const stream = new ReadableStream({
+    //   async start(controller) {
+    //     for await (const chunk of results) {
+    //       if (
+    //         chunk.type === 'response.output_text.delta' &&
+    //         chunk.delta
+    //       ) {
+    //         controller.enqueue(new TextEncoder().encode(chunk.delta))
+    //       }
+    //     }
+
+    //     controller.close()
+    //   }
+    // })
+
+    // return new Response(stream, {
+    //   headers: {
+    //     Connection: 'Keep-Alive',
+    //     'Content-Encoding': 'none',
+    //     'Cache-Control': 'no-cache, no-transform',
+    //     'Content-Type': 'text/event-stream; charset=utf-8',
+    //   }
+    // })
+
     res.status(200).json(results.output_text)
   } catch (error) {
     if (error instanceof Error) {
@@ -66,16 +107,13 @@ server.post('/api/v1/chat', validateInput(chatInputSchema), async (
     res.status(500).json({ msg: 'Internal Server Error.' })
   }
 })
-/**
- * End API Routes
- */
-
 
 /**
  * Main function that starts the express server.
  */
 function main(): void {
-  server.listen(PORT, () => {
+  server.listen(PORT, async () => {
+    await connectDatabase()
     console.log(`server started on http://localhost:${PORT}`)
   })
 }
