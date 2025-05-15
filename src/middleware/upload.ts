b1494a080e25ec76e3f6811276b888da034b6dd4
Jesse Emana <jesseemana@gmail.com>
2025-05-15 15:46:53 +0200

    REFACTOR: changed database name

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,13 +1,38 @@
-import multer from 'multer'
+import multer, { FileFilterCallback } from 'multer'
 import path from 'path'
+import fs from 'fs-extra'
+import { Request } from 'express'
 
 const storage = multer.diskStorage({
-  destination: function(_req, _file, cb) {
-    cb(null, 'uploads/')
+  destination: function(
+    _req: Request,
+    _file: Express.Multer.File,
+    cb: (error: Error | null, destination: string) => void
+  ) {
+    const uploadDir = 'uploads'
+    fs.ensureDirSync(uploadDir)
+    cb(null, uploadDir)
   },
   filename: function(_req, file, cb) {
     cb(null, `Doc${'-'}${Date.now() + path.extname(file.originalname)}`)
   }
 })
 
-export const upload = multer({ storage: storage })
+const fileFilter = (_req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
+  if (file.fieldname === 'file') {
+    const allowedFileTypes = /pdf/
+    const extension = path.extname(file.originalname).toLowerCase()
+    const isAllowed = allowedFileTypes.test(extension)
+    if (!isAllowed) {
+      // @ts-ignore
+      return cb(new Error('File type not allowed for attachment!'), false)
+    }
+  }
+  cb(null, true)
+}
+
+export const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 50 * 1024 * 1024 },
+})
--- src/utils/database.ts
+++ src/utils/database.ts
@@ -3,8 +3,9 @@ import mongoose from 'mongoose'
 export const connectDatabase = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI as string, {
-      dbName: 'MarkDown',
+      dbName: 'GuideBook',
       maxPoolSize: 10,
+      autoIndex: false,
     })
     console.log('Database Connected!!!')
   } catch (error) {
